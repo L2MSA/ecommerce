1dc9c534b331e8e5b5651e764cd5204c7bfce67d
Leonardo Pianta <leo.a.pianta@gmail.com>
2018-05-20 19:27:43 -0300

    Calculos de preços minimo e de venda
    Implementação de rotinas de calculo de preço minimo e preço com impostos (preço de venda).
    Tambem foi corrigida a atualização de estoque.

--- AulaModelo/Controllers/ProdutoController.cs
+++ AulaModelo/Controllers/ProdutoController.cs
@@ -48,53 +48,90 @@ namespace AulaModelo.Controllers
         public ActionResult GravarProduto(Produto produto, Guid IdCategoria)
         {
 
-                ViewBag.IdCategoria = new SelectList(
-                DbFactory.Instance.CategoriaRepository.FindAll(),
-                "Id",
-                "Nome",
-                IdCategoria
-            );
-            
+            ViewBag.IdCategoria = new SelectList(
+            DbFactory.Instance.CategoriaRepository.FindAll(),
+            "Id",
+            "Nome",
+            IdCategoria
+        );
+
             if (produto.Id.ToString() == "00000000-0000-0000-0000-000000000000")
             {
-                    var categoria = DbFactory.Instance.CategoriaRepository.FindById(IdCategoria);
-                    produto.Categoria = categoria;
-                    DbFactory.Instance.ProdutoRepository.SaveOrUpdate(produto);
-                    Estoque estoque = new Estoque()
-                    {
-                        Produto = produto,
-                        PrecoAtual = produto.Preco,
-                        Quantidade = produto.Estoque.Quantidade
+                var categoria = DbFactory.Instance.CategoriaRepository.FindById(IdCategoria);
+                produto.Categoria = categoria;
+                DbFactory.Instance.ProdutoRepository.SaveOrUpdate(produto);
+                Estoque estoque = new Estoque()
+                {
+                    Produto = produto,
+                    PrecoAtual = produto.Preco,
+                    Quantidade = produto.Estoque.Quantidade
 
-                    };
-                    DbFactory.Instance.EstoqueRepository.SaveOrUpdate(estoque);
+                };
+                DbFactory.Instance.EstoqueRepository.SaveOrUpdate(estoque);
             }
             else
             {
-            var categoria = DbFactory.Instance.CategoriaRepository.FindById(IdCategoria);
-            produto.Categoria = categoria;
+                var categoria = DbFactory.Instance.CategoriaRepository.FindById(IdCategoria);
+                produto.Categoria = categoria;
 
-            Estoque estoque = new Estoque()
-            {
-                Produto = produto,
-                PrecoAtual = produto.Preco,
-                Quantidade = produto.Estoque.Quantidade
+                Estoque estoque = new Estoque()
+                {
+                    Produto = produto,
+                    PrecoAtual = produto.Preco,
+                    Quantidade = produto.Estoque.Quantidade
 
-            };
+                };
 
-            var e = DbFactory.Instance.EstoqueRepository.buscarporIdProduto(produto.Id);
-            estoque = e;
-            estoque.Quantidade = produto.Estoque.Quantidade;
-            estoque.PrecoAtual = produto.Preco;
-            var p = DbFactory.Instance.ProdutoRepository.FindById(produto.Id);
-            produto.Preco = p.Preco;
-            DbFactory.Instance.ProdutoRepository.SaveOrUpdate(produto);
-            DbFactory.Instance.EstoqueRepository.SaveOrUpdate(estoque);
+                var e = DbFactory.Instance.EstoqueRepository.buscarporIdProduto(produto.Id);
+                var p = DbFactory.Instance.ProdutoRepository.FindById(produto.Id);
+
+                var precoAtual = p.Preco;
+                var PrecoEstoqueNovo = produto.Preco;
+                var estoqueAtual = e.Quantidade;
+                var estoqueNovo = produto.Estoque.Quantidade;
+
+                var PrecoMinimo = CalcPrecoMinimo(precoAtual, PrecoEstoqueNovo, estoqueAtual, estoqueNovo);
+
+                estoque = e;
+                estoque.Quantidade += produto.Estoque.Quantidade;
+                estoque.PrecoAtual = Math.Round(produto.Preco, 2);
+                produto.Estoque = estoque;
+                
+
+                produto.Preco = CalcPrecoImpostos(PrecoMinimo);
+
+                
+
+                DbFactory.Instance.ProdutoRepository.SaveOrUpdate(produto);
+                DbFactory.Instance.EstoqueRepository.SaveOrUpdate(produto.Estoque);
             }
 
 
 
             return RedirectToAction("Index", "Home");
+        }
+
+        public double CalcPrecoMinimo(double precoAtual, double PrecoEstoqueNovo, double estoqueAtual, double estoqueNovo)
+        {
+            double PrecoMinimo = ((estoqueAtual * precoAtual) + (estoqueNovo * PrecoEstoqueNovo)) / (estoqueAtual + estoqueNovo);
+            return Math.Round(PrecoMinimo, 2);
+        }
+
+        public double CalcPrecoImpostos(double precoMinimo)
+        {
+            double precoVenda;
+            double despVariavel = 5.60;
+            double despFixa = 3;
+            double margLucro = 20;
+            double Impostos = 36.25;
+
+            var totalImpostos = despVariavel + despFixa + margLucro + Impostos;
+
+            precoVenda = precoMinimo * (1 + (totalImpostos / 100));
+
+           
+
+            return Math.Round(precoVenda, 2);
         }
 
         public ActionResult ApagarProduto(Guid id)
@@ -172,7 +209,7 @@ namespace AulaModelo.Controllers
         }
         public ActionResult adicionaraoCarrinho(Guid id)
         {
-            
+
             var produto = DbFactory.Instance.ProdutoRepository.FindById(id);
             //Produto preenchido//
             Usuario usuario = new Usuario();
@@ -182,7 +219,7 @@ namespace AulaModelo.Controllers
             carrinho.Produto = produto;
             carrinho.Usuario = usuario;
             var addCarrinho = DbFactory.Instance.CarrinhoRepository.Save(carrinho);
-            return RedirectToAction("ExibirProduto/"+id);
+            return RedirectToAction("ExibirProduto/" + id);
         }
         public ActionResult viewCarrinho()
         {
